489a9759b9ae9de0b135f721e8abc8a49ae901de
Joshua Basche <joshbasche@live.com>
2021-09-02 13:40:38 -0700

    add visible time to Countdown

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Countdown } from '../../components/Countdown';
 import { colors } from '../../utils/colors';
 import { spacing } from '../../utils/sizes';
 
 export const Timer = ({ focusSubject }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.countdown}>
+        <Countdown />
+      </View>
+
       <View style={{ paddingTop: spacing.xxl }}>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
@@ -26,5 +31,10 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  countdown: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
